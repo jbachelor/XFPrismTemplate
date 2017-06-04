f1ab5d057e62aafefe680e2e30c93897a7242360
Jonathan Bachelor <jon_bachelor@me.com>
2017-06-04 15:23:38 -0700

    Fully updated (as of 2017-06-04) base Prism app for Xamarin.Forms, supporting iOS and Android.

---
+++ XFPrismTemplate/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Unity;
+using XFPrismTemplate.Views;
+
+namespace XFPrismTemplate
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync(nameof(MainPage));
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<MainPage>();
+		}
+	}
+}
+
